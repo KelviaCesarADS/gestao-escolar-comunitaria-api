686e297f448fa925fc6eed0037da9dbc19295015
Czcatao <cczc@cesar.school>
2025-11-05 21:05:33 -0300

    Implement basic CRUD operations for professors

--- modulos/professores/crud_professores.py
+++ modulos/professores/crud_professores.py
@@ -1,2 +1,51 @@
+professor = []
+
+def menu():
+    print("---------SISTEMA OPERACIONAL----------")
+    print("\n1- Adicionar professor..")
+    print("2- Deletar professor..")
+    print("3- Listar professores..")
+    print("4- Atualizar professroes..")
+    print("5- SAIR DO SISTEMA")
+    escolha = int(input("\nEscolha oque voce quer fazer de acordo com a numeração: "))
+    return escolha
+
+
+while True:
+    match (menu()):
+        case 1:
+            prof = {}
+            print("sistema: ADD PROFESSOR...")
+            prof["nome"] = input("\nDigite o nome desse professor: ")
+            prof["idade"] = int(input("Digite sua idade: "))
+            prof["materia"] = input("Digite a area que ele atua: ")
+            professor.append(prof)
+
+            print ("\n ----- Dados dos Alunos -----")
+            print(f"Nome: {prof['nome']}")
+            print(f"Idade: {prof['idade']}")
+            print(f"Materia: {prof['materia']}")
+            print("\n PROFESSOR ADICIONADO!!!!")
+
+            cnt = input("Voce deseja continuar (S/N):")
+            if cnt == "n" or "N":
+                print("Fechando sistema....")
+                break
+            elif cnt == "S" or "s":
+                continue
+            else:
+                print("Er00r...")
+        case 2:
+            print("sistema: DELET PROFESSOR...")
+            deletar = ("deseja deletar o ultimo professor cadastrado??")
+            professor.pop(deletar)
+            print("Professor deletado do sistema!!!")
+            cnt = input("Voce deseja continuar (S/N):")
+            if cnt == "n" or "N":
+                print("Fechando sistema....")
+                break
+            elif cnt == "S" or "s":
+                continue
+            
 
 
