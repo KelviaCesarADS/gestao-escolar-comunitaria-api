b13869d1063b2491abaa084743ea7866b3b85473
Pris <54074427+Pris-Pontes@users.noreply.github.com>
2025-11-12 12:54:57 -0300

    delete e inicio do update para o crud de turmas

---
+++ modulos/turmas/crud_turmas.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+ARQUIVO_PASTA = os.path.join(os.path.dirname(__file__), "turmas.json")
+
+def carregar_turmas():
+    if os.path.exists(ARQUIVO_PASTA):
+        with open(ARQUIVO_PASTA, "r", encoding="utf-8") as t:
+            return json.load(t)
+    return[]
+
+def salvar_turmas():
+    if os.path.exists(ARQUIVO_PASTA):
+        with open(ARQUIVO_PASTA, "w", encoding="utf-8") as t:
+            return json.load(t)
+    return[]
+
+
+def deletar_turma(turmas):
+    carregar_turmas(turmas)
+    if not turmas:
+        print(f"Não há turmas cadastradas ainda.\n")
+        return
+    try:
+        id_turma = int(input("Iforme o ID da turma que será excluída: "))
+        for t in turmas:
+            if t["id"] == id_turma:
+                confirmar = input(f"Tem certeza que deseja excluir a turma '{t['ano_serie']}' (ID {id_turma})? (s/n): ").strip().lower()
+                if confirmar == 's':
+                    turmas.remove(t)
+                    salvar_turmas(turmas)
+                    print(f"A turma '{t['ano_serie']}' removida com sucesso!\n")
+                    return
+                else:
+                    print(f"Operação cancelada.\n")
+        print(f"Essa turma não foi encontrada.\n")
+    except ValueError:
+        print(f"Este ID é inválido.\n")
+
+def atualizar_turma(turmas):
+    carregar_turmas(turmas)
+    if not turmas:
+        print(f"Não há turmas cadastradas ainda.\n")
+        return
+    try:
+        id_turma = int(input("Informe o ID da turma que será atualizada: "))
+        for t in turmas:
+            if t["id"] == id_turma:
+                print(f"Editando: {t["Ano ou Serie da turma: "]}")
+                salvar_turmas(turmas)
+                print(f"A turma foi atualizada com sucesso!\n")
+                return
+        print(f"Essa turma não foi encontrada.\n")
+    except ValueError:
+        print(f"Este ID é inválido.\n")
